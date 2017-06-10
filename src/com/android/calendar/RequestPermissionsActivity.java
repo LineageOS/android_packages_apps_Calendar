3354465063ec297b7375d1907b226073650a14c2
Demon000 <demonsingur@gmail.com>
2017-06-11 01:14:02 +0300

    Calendar: request external storage permissions
    External storage permissions are needed for exporting and importing.
    
    Change-Id: Ibc76435748c03d16516cd0b38eeeef70289024b8

--- src/com/android/calendar/RequestPermissionsActivity.java
+++ src/com/android/calendar/RequestPermissionsActivity.java
@@ -28,8 +28,10 @@ import android.app.Activity;
 public class RequestPermissionsActivity extends RequestPermissionsActivityBase {
 
     private static final String[] REQUIRED_PERMISSIONS = new String[] {
-    // "Calendar" group. Without this permission.
-    permission.READ_CALENDAR };
+        permission.READ_CALENDAR, // Calendar group
+        permission.READ_EXTERNAL_STORAGE,
+        permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected String[] getRequiredPermissions() {
